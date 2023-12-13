44e5402deabdb8954c8f3edc2b3eb6f365f1a5b8
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-13 14:34:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i=0;i<str.length){
+		if(str[i]==str[i+1]){
+			delete str[i];
+			delete str[i+1];
+		}
+		else{
+			i++;
+		}
+	}
+	return str;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
